f1b3e3549a00a1ea43c4702df608fe29c5acdfba
woyue2 <124415197+woyue2@users.noreply.github.com>
2023-10-08 00:22:21 +0800

    更新 script.js

--- 01ExpandingCards/script.js
+++ 01ExpandingCards/script.js
@@ -1,4 +1,6 @@
+//选中了panel 和 panel active
 const panelItems = document.querySelectorAll(".container > .panel");
+
 panelItems.forEach(item => {
     item.addEventListener('click',() => {
         [].filter.call(item.parentElement.children,el => el !== item).forEach(el => el.classList.remove('active'));
